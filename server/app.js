edf4497079fc6ca468bbeb036cf8a10d81c4079e
RF4TH4N11 <almujahid1204@gmail.com>
2025-08-27 16:27:29 +0700

    refactor app feat server and refactor socket

--- server/app.js
+++ server/app.js
@@ -1,68 +1,28 @@
 const express = require("express");
 const { createServer } = require("node:http");
-const { Server } = require("socket.io");
-
-const app = express();
-const httpServer = createServer(app);
 const cors = require("cors");
+
+const { initSocket } = require("./socket/socket");
 const errorHandler = require("./middlewares/error-handler");
 const router = require("./routers/route");
 
-const PORT = 3000;
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
-});
-
+const app = express();
+app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+const httpServer = createServer(app);
 
-  socket.on("join-board", ({ BoardId }) => {
-    if (!BoardId) return;
-    socket.join(`board:${BoardId}`);
-    console.log(`User ${socket.id} joined board:${BoardId}`);
-  });
+const { broadcast } = initSocket(httpServer, {
+  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+});
 
-  socket.on("leave-board", ({ BoardId }) => {
-    if (!BoardId) return;
-    socket.leave(`board:${BoardId}`);
-    console.log(`User ${socket.id} left board:${BoardId}`);
-  });
-
-  socket.on("task:create", (data) => {
-    socket.to(`board:${data.BoardId}`).emit("task:created", data);
-  });
-  socket.on("task:update", (data) => {
-    socket.to(`board:${data.BoardId}`).emit("task:updated", data);
-  });
-  socket.on("task:delete", (data) => {
-    socket.to(`board:${data.BoardId}`).emit("task:deleted", data);
-  });
-  socket.on("tasks:reorder", (data) => {
-    socket.to(`board:${data.BoardId}`).emit("tasks:reordered", data);
-  });
-  socket.on("tasks:seed", (data) => {
-    socket.to(`board:${data.BoardId}`).emit("tasks:seeded", data);
-  });
-  socket.on("attachment:create", (data) => {
-    socket.to(`board:${data.BoardId}`).emit("attachment:created", data);
-  });
-  socket.on("attachment:delete", (data) => {
-    socket.to(`board:${data.BoardId}`).emit("attachment:deleted", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+app.use((req, res, next) => {
+  req.broadcast = broadcast;
+  next();
 });
 
 app.use(router);
-
 app.use(errorHandler);
 
-httpServer.listen(PORT, () => console.log("Socket server on", PORT));
+module.exports = { app, httpServer };
