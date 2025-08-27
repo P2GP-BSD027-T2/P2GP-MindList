660e55c76123486d405c01d0eae3da73a380f478
faqq11 <faqihazizy7@gmail.com>
2025-08-28 00:52:49 +0700

    feat: add all tasks endpoint

--- server/middlewares/error-handler.js
+++ server/middlewares/error-handler.js
@@ -19,9 +19,6 @@ const errorHandler = (err, req, res, next) => {
   } else if (err.message === "BOARD_NOT_FOUND") {
     statusCode = 404;
     message = "Board not found.";
-  } else if (err.message === "TASKS_NOT_FOUND") {
-    statusCode = 404;
-    message = "Tasks not found.";
   }
 
   res.status(statusCode).json({
--- server/routers/route.js
+++ server/routers/route.js
@@ -18,10 +18,10 @@ router.post("/boards/:id/invite", BoardController.getCode);
 
 // Tasks
 router.get("/boards/:id/tasks", TaskController.getAllTasks);
-router.post("/boards/:id/tasks", TaskController.Task);
-router.patch("/boards/:id/tasks/:taskId", TaskController.Task);
-router.delete("/boards/:id/tasks/:taskId", TaskController.Task);
-router.put("/boards/:id/tasks/reorder", TaskController.Task);
+router.post("/boards/:id/tasks", TaskController.addTask);
+router.patch("/boards/:id/tasks/:taskId", TaskController.editTask);
+router.delete("/boards/:id/tasks/:taskId", TaskController.deleteTask);
+router.put("/boards/:id/tasks/reorder", TaskController.reorderTasks);
 
 // Attachments
 router.post(
